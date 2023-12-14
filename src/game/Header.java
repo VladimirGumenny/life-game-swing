e941af0ff1743f6634cb0a561a01881427eba04f
vgumennyy <vladimir.gumennyy@gmail.com>
2023-12-13 19:15:21 -0800

    fix updating of step number

--- src/game/Header.java
+++ src/game/Header.java
@@ -8,21 +8,25 @@ import static game.Board.COLUMNS;
 import static game.Board.TILE_SIZE;
 
 public class Header extends JPanel {
+    public static final String STEP_LABEL_MESSAGE = "Step: ";
     private int step = 0;
+    private JLabel stepLabel;
 
     public Header() {
         setPreferredSize(new Dimension(TILE_SIZE * COLUMNS, TILE_SIZE * 2));
+
+        stepLabel = new JLabel(STEP_LABEL_MESSAGE + step);
+        Font newFont = new Font(stepLabel.getFont().getName(), Font.PLAIN, 20);
+        stepLabel.setFont(newFont);
+        stepLabel.setOpaque(true);
+        add(stepLabel);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        var stepLable = new JLabel("Step: " + step);
-        Font newFont = new Font(stepLable.getFont().getName(), Font.PLAIN, 20);
-        stepLable.setFont(newFont);
-        stepLable.setOpaque(true);
-        add(stepLable);
+        stepLabel.setText(STEP_LABEL_MESSAGE + step);
 
         // this smooths out animations on some systems
         Toolkit.getDefaultToolkit().sync();
