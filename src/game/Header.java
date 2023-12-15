e3d890d7c1a2d121cde551d74b5bfe6e5259354e
vgumennyy <vladimir.gumennyy@gmail.com>
2023-12-14 19:39:38 -0800

    pause by space bar added

--- src/game/Header.java
+++ src/game/Header.java
@@ -1,7 +1,6 @@
 package game;
 
 import javax.swing.*;
-
 import java.awt.*;
 
 import static game.Board.COLUMNS;
@@ -9,15 +8,19 @@ import static game.Board.TILE_SIZE;
 
 public class Header extends JPanel {
     public static final String STEP_LABEL_MESSAGE = "Step: ";
+    public static final Color BACKGROUND_COLOR = Color.LIGHT_GRAY;
     private int step = 0;
     private final JLabel stepLabel;
 
     public Header() {
         setPreferredSize(new Dimension(TILE_SIZE * COLUMNS, TILE_SIZE * 2));
+        setBackground(BACKGROUND_COLOR);
 
         stepLabel = new JLabel(STEP_LABEL_MESSAGE + step);
-        Font newFont = new Font(stepLabel.getFont().getName(), Font.PLAIN, 20);
-        stepLabel.setFont(newFont);
+        stepLabel.setForeground(Color.BLUE);
+        stepLabel.setBackground(BACKGROUND_COLOR);
+        Font font = new Font(stepLabel.getFont().getName(), Font.BOLD, 20);
+        stepLabel.setFont(font);
         stepLabel.setOpaque(true);
         add(stepLabel);
     }
